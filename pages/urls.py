ee00009c83b7b3e95c17cb34efd4608d08a75077
elihoole <elihoole@gmail.com>
2023-03-02 17:55:19 +0530

    change base.html to display judgements

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, CaseListView
 
 urlpatterns = [
+    path(
+        "supreme_court_judgements/",
+        CaseListView.as_view(),
+        name="supreme_court_judgements",
+    ),
     path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
-    path("supreme_court_cases/", CaseListView.as_view(), name="supreme_court_cases"),
 ]
